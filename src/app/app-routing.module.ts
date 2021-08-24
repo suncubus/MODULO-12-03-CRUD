38ddbcc91430b4bdda9e8a5b0f15ca434aaf7b21
Patri <witty.free@gmail.com>
2021-08-24 11:16:15 +0200

    añadir las rutas de los componentes a app-routing.module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+//componentes
+import { UsersComponent } from './users/users.component';
+import { AdduserComponent } from './adduser/adduser.component';
+
+const routes: Routes = [
+  { path: '', component: UsersComponent },
+  { path: 'users', component: UsersComponent },
+  { path: 'adduser', component: AdduserComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
