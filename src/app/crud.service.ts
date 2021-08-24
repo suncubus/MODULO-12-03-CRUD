dc20fc9393c188ef780054f15ed230337d1beb29
Patri <witty.free@gmail.com>
2021-08-24 11:21:59 +0200

    añadir htmmenu html en app.component

--- src/app/crud.service.ts
+++ src/app/crud.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CrudService {
+  userData:any;
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  //get all users  details
+  public getusers(){        
+          return this.http.get('http://localhost/users.php');
+  }
+
+  //add new user    
+  public adduser(userData:any){
+      /*return this.http.post('http://localhost/users.php/', userData)
+        .subscribe(
+          (res: Response) => {
+              this.getusers();
+        });*/
+      }
 }
