1ba048b6c2c28c44bd05ba1414e8b31c8b1029d4
Patri <witty.free@gmail.com>
2021-08-24 11:06:12 +0200

    crear componentes user y adduser

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UsersComponent } from './users/users.component';
+import { AdduserComponent } from './adduser/adduser.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    AdduserComponent
   ],
   imports: [
     BrowserModule,
